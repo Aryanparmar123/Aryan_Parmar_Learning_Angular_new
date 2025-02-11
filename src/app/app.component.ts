67d9465bb3b667219df6bc5fad4352a436bb39a3
Aryanparmar123 <W0848380@myscc.ca>
2025-02-11 18:54:58 -0500

    “Created starter Angular file and completed Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'AngularProject111';
+  name = 'aryan parmar';
+
+  aboutMe = 'hi i am a student here at st clair college'
 }
