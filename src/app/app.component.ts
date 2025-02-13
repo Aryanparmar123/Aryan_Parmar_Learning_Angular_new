2dbd9d9811c48cfaf562ef58d37c0a000dbbd1b9
Aryanparmar123 <W0848380@myscc.ca>
2025-02-12 21:22:37 -0500

    add div tag and structural directives. Done Assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Product } from './models/product';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularProject111';
-  name = 'aryan parmar';
-
-  aboutMe = 'hi i am a student here at st clair college'
+  products: Product[] = [
+    { id: 1, name: "Laptop", price: 120, category: "Electronics", color: "Black" },
+    { id: 2, name: "Phone", price: 8900, category: "Electronics", color: "Black" },
+    { id: 3, name: "Chair", price: 1250, category: "Furniture", color: "White" }
+  ];
 }
