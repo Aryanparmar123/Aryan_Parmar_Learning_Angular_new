00921d8e2ba0b5c6508a2ce4257dea49e631332d
Aryanparmar123 <W0848380@myscc.ca>
2025-02-19 22:34:59 -0500

    Assignment 4 step 1 package version update

--- src/app/content-list-item/content-list-item.component.ts
+++ src/app/content-list-item/content-list-item.component.ts
@@ -1,13 +1,17 @@
 import {Component, Input} from '@angular/core';
 import {Product} from '../models/product';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-content-list-item',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './content-list-item.component.html',
   standalone: true,
   styleUrl: './content-list-item.component.css'
 })
 export class ContentListItemComponent {
   @Input() product!: Product;
+
 }
