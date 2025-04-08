64cf361a56f1fff7b5ea9d5fa49a90c90b72bd4f
Aryanparmar123 <W0848380@myscc.ca>
2025-04-07 23:53:38 -0400

    added all the methods in the service file

--- src/app/services/electronic-items.service.ts
+++ src/app/services/electronic-items.service.ts
@@ -7,7 +7,7 @@ import {Product} from '../Shared/models/product';
   providedIn: 'root'
 })
 export class ElectronicItemsService {
-private product:Product[]= ProductList;
+private products:Product[]= ProductList;
   constructor() { }
 
 
@@ -15,4 +15,36 @@ private product:Product[]= ProductList;
     return of(ProductList);
   }
 
+
+  getElectronicsById(id: number): Observable<Product | undefined> {
+    const product = this.products.find(item => item.id === id);
+    return of(product);
+  }
+
+  addElectronics(newProduct: Product): Observable<Product[]> {
+    this.products.push(newProduct);
+    return of(this.products);
+  }
+
+  updateElectronics(updatedProduct: Product): Observable<Product[]> {
+    const index = this.products.findIndex(product => product.id === updatedProduct.id);
+    if (index !== -1) {
+      this.products[index] = updatedProduct;
+    }
+    return of(this.products);
+  }
+
+  deleteElectronics(productId: number): Observable<Product | undefined> {
+    const index = this.products.findIndex(product => product.id === productId);
+    if (index !== -1) {
+      const removedProduct = this.products.splice(index, 1)[0];
+      return of(removedProduct);
+    }
+    return of(undefined);
+  }
 }
+
+
+
+
+
