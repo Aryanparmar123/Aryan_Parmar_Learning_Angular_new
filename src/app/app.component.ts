a1c739953a848c7501c11d0a4fd2aadc8d7e0668
Aryanparmar123 <W0848380@myscc.ca>
2025-04-08 00:16:59 -0400

    asssignment 4 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ContentListComponent} from './content-list/content-list.component';
+import {ContentListItemComponent} from './content-list-item/content-list-item.component';
+import {ElectronicItemsService} from './services/electronic-items.service';
+import {Product} from './Shared/models/product';
 
 @Component({
   selector: 'app-root',
-  imports: [ContentListComponent],
+  imports: [ContentListComponent, ContentListItemComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  product?: Product;
+
   title = 'Launched Products';
+
+
+  constructor(private electronicItemsService: ElectronicItemsService) { }
+
+  ngOnInit(): void {
+    this.getItemById(5);
+  }
+
+  getItemById(id: number): void {
+    this.electronicItemsService.getElectronicsById(id).subscribe(item => {
+      this.product = item;
+    });
+  }
 }
